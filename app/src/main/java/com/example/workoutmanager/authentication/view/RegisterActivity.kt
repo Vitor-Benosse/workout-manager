1b7077de338f1d80a50d0f04becfb4d6806caa69
Vitor Benosse <vitorbenosse@gmail.com>
2021-02-15 22:24:07 -0400

    Adding register layout

---
+++ app/src/main/java/com/example/workoutmanager/authentication/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.workoutmanager.authentication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workoutmanager.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
